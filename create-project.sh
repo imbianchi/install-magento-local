bffb930f882baac6711df3f86f3128d415b052dd
local <joaobianchi@live.com>
2021-06-16 01:52:22 -0300

    :construction: fix git configs && magento cmds

--- create-project.sh
+++ create-project.sh
@@ -4,4 +4,4 @@ mkdir -p ./$STORENAME/htdocs
 cd $STORENAME/htdocs
 sudo -H -u www-data bash -c "git clone $GITREPO ."
 sudo -H -u www-data bash -c "git checkout $GITBRANCH -f"
-composer install
+sudo -H -u www-data bash -c "composer install"
--- install-store.sh
+++ install-store.sh
@@ -13,10 +13,12 @@ read GITREPO
 echo -n "DB name: "
 read DBNAME
 
+echo -n "DB password: "
+read DBPSWD
+
 export STORENAME
 export STOREURL
 export DBPSWD
-export ADMINPSWD
 export DBNAME
 
 bash ./create-project.sh
