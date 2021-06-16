bcb842a128ed10860a64ef3622141646d83bbb40
local <joaobianchi@live.com>
2021-06-16 02:39:55 -0300

    :construction: dealing with git

--- create-project.sh
+++ create-project.sh
@@ -2,6 +2,6 @@
 cd /shared/httpd/
 sudo mkdir -p $STORENAME/htdocs
 cd $STORENAME/htdocs
-sudo bash -c "git clone $GITREPO"
+sudo bash -c "git clone $GITREPO ."
 sudo bash -c "git checkout $GITBRANCH -f"
 sudo bash -c "composer install"
